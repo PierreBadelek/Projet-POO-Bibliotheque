3c742b446ae54aeefeed4970fc29eb47337d7f72
SPUPHF <sean.persand@uphf.fr>
2025-11-24 16:07:29 +0100

    Ajout de nouveaux fichiers

---
+++ src/main/java/com/example/projetpoobibliotheque/BibliothequeApp.java
@@ -0,0 +1,22 @@
+package com.example.projetpoobibliotheque;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BibliothequeApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainView.fxml"));
+        Scene scene = new Scene(loader.load(), 450, 350);
+        primaryStage.setTitle("Bibliothèque POO 3A");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
